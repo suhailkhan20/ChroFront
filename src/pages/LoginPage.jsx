275b651cc53bb2dbf36dddd50d943431edbe4836
Suhail khan <suhailkhan2k5p@gmail.com>
2025-09-08 12:03:27 +0530

    Update

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5020/api/auth/login', {
+      const response = await fetch('https://chrolog.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
